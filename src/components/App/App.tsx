8bae8f0af13f80f0d5a0e63e2764b9df157f1383
Volodymyr Salashchenko <salashchenkovladymyr@gmail.com>
2026-01-18 13:33:50 +0200

    add content MovieGrid

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,16 @@ import css from "./App.module.css"
 import SearchBar from "../SearchBar/SearchBar"
 import toast,{Toaster} from "react-hot-toast";
 import type { Movie } from "../../types/movie";
+import { fetchMovies } from "../../services/movieService";
+import { useState } from "react";
+import MovieGrid from "../MovieGrid/MovieGrid";
 
 
 export default function App() {
-   
-  // Функція, яка буде спрацьовувати при натисканні на кнопку пошуку
+   const [movies, setMovies] = useState<Movie[]>([]);
   const handleSearch = async (topic: string) => {
-    const movies: Movie[] = [];
-    if (movies.length === 0) {
+    const data = await fetchMovies(topic);
+    if (data.length === 0) {
   toast.error("No movies found for your request.", {
                 style: {
                     border: '1px solid #713200',
@@ -20,14 +22,20 @@ export default function App() {
   });        
 }
     console.log("Шукаємо фільм:", topic);
+    setMovies(data);
   };
-
+const handleSelectMovie = (movie: Movie) => {
+    console.log("Вибрано фільм для перегляду:", movie.title);
+  };
   return (
     <div className={css.appContainer}>
       <Toaster
         position="top-center"
         reverseOrder={false} />
       <SearchBar onSubmit={handleSearch} />
+      {movies.length > 0 && (
+        <MovieGrid movies={movies} onSelect={handleSelectMovie} />
+      )}
     </div>
   );
   
--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -1,1 +1,31 @@
-// import css from "./MovieGrid.module.css"
+import type { Movie } from "../../types/movie"
+import css from "./MovieGrid.module.css"
+
+interface MovieGridProps {
+  movies: Movie[];
+  onSelect: (movie: Movie) => void; 
+}
+
+export default function MovieGrid({ movies, onSelect }: MovieGridProps) {
+    return (
+        <ul className={css.grid}>
+            {movies.map((movie) => (
+                <li key={movie.id}>
+                    <div className={css.card} onClick={() => onSelect(movie)}>
+                        {movie.poster_path ? (
+                            <img
+                                className={css.image}
+                                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                alt={movie.title}
+                                loading="lazy"
+                            />
+                        ) : (
+                            <div className={css.noImage}>No Image</div>
+                        )}
+                        <h2 className={css.title}>{movie.title}</h2>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    );
+}
